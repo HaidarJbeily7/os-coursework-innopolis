029a7d90c70e9fd37cbcf772a50883ece8b49ef5
Haidar Jbeily <h.jbeily@innopolis.university>
2023-09-12 22:25:20 +0300

    week01

--- week1/ex3.sh
+++ week1/ex3.sh
@@ -1,9 +1,33 @@
+echo "Creating root folder..."
 date
 sleep 3
 mkdir ./root
-ls / > "root/root.txt" 
 
+
+echo "Creating root file..."
+date
+sleep 3
+ls -ltr / > "root/root.txt" 
+
+echo "Creating home folder..."
 date
 sleep 3
 mkdir ./home
-ls ~ > "home/home.txt" 
+
+echo "Creating home file..."
+date
+sleep 3
+ls -ltr ~ > "home/home.txt" 
+
+
+echo "Content of root.txt:"
+cat root/root.txt
+
+echo "Content of home.txt:"
+cat home/home.txt
+
+echo "Content of home directory:"
+ls -ltr home
+
+echo "Content of root directory:"
+ls -ltr root
