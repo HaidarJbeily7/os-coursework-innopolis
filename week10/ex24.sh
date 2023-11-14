6e935824a2a00936ac948ed3b9b5f0739651c690
Haidar Jbeily <h.jbeily@innopolis.university>
2023-11-14 20:18:46 +0300

    week10
    Co-authored-by: HaidarJbeily7 <h.jbeily@innopolis.university>

--- week10/ex24.sh
+++ week10/ex24.sh
@@ -1,33 +1,43 @@
 #!/bin/bash
 
-
+echo "Delete ./tmp if exists"
 [ -d "./tmp" ] && rm -r ./tmp
+
+echo "\n\nCreating symbolic link tmp1 to ./tmp:\n"
 ln -s ./tmp tmp1
 
-echo "Running ls -li before creating ./tmp:"
+echo "\nRunning ls -li before creating ./tmp:"
 ls -li
 
 mkdir ./tmp
 
-echo "Running ls -li after creating ./tmp:"
+echo "\nRunning ls -li after creating ./tmp:"
 ls -li
+
 
 echo "\n\nWhat is the diffrence? The diiference that after we created the ./tmp directory, the symbolic link is now pointing to the new directory and usable but before that this was a broken link.\n\n"
 
 
+echo "\n\nAdding ex1.txt file to ./tmp:"
+chmod -x ./gen.sh
 ./gen.sh 10 ./tmp/ex1.txt
 
+echo "\n\nChecking the content of ./tmp1:"
+ls -li ./tmp1
 
-ln -s ./tmp1/tmp2
 
+echo "\n\nAdding symbolic link to ./tmp:"
+ln -s "$(realpath .)/tmp" ./tmp1/tmp2
 
 ./gen.sh 10 ./tmp1/tmp2/ex1.txt
 
 
-echo "Checking the content of ./tmp1/tmp2:"
-ls ./tmp1/tmp2
+echo "\n\nChecking the content of ./tmp1/tmp2/tmp2/tmp2:"
+ls ./tmp1/tmp2/tmp2/tmp2
 
 rm -r ./tmp
 
-echo "Checking symbolic links after deleting ./tmp:"
+echo "\n\nChecking symbolic links after deleting ./tmp:"
 ls -l tmp1
+
+rm -r ./tmp1
